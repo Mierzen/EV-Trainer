b8c7c17858aa9eda45441920855581329c90b061
Mierzen <steptaljaard@gmail.com>
2014-12-08 09:40:29 +0200

    Fix selection of new Pokémon after it has been added

--- EV Trainer/EV Trainer/saveData.vb
+++ EV Trainer/EV Trainer/saveData.vb
@@ -63,6 +63,8 @@ Module saveData
         Dim info As Byte() = New UTF8Encoding(True).GetBytes(str)
         fs.Write(info, 0, info.Length)
         fs.Close()
+
+        trainPokemonList()
     End Sub
 
     Public Sub loadStats(pok As String)
